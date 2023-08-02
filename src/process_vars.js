d1764d66166ab65cd6a98f42bb2040aea02d7482
Michael Kreil <github@michael-kreil.de>
2023-08-02 13:17:19 +0200

    switch to list of variants

--- src/process_vars.js
+++ src/process_vars.js
@@ -1,4 +1,48 @@
 
 import { argv } from 'node:process';
 
-console.log(argv)
+let args = JSON.parse(argv[2]);
+let keys = 'filename,platforms,repo,tag,variants'.split(',');
+
+
+
+// check if all keys are valid
+Object.keys(args).forEach(key => {
+	if (!keys.includes(key)) throw Error(`unknown key "${key}"`)
+})
+
+// cleanup values, set to undefined if not defined
+keys.forEach(key => {
+	if (typeof args[key] !== 'string') return args[key] = undefined;
+	args[key] = args[key].trim();
+	let value = args[key].toLowerCase();
+	if ((value === '') || (value === 'false')) return args[key] = undefined;
+})
+
+// check undefined values
+if (!args.filename) throw Error('filename not defined');
+args.platforms ??= 'linux/amd64,linux/arm64';
+args.repo ??= 'versatiles';
+if (!args.tag) throw Error('tag not defined');
+args.variants ??= '';
+
+// calc suffixes
+let suffixes = args.variants.split(',').map(v => {
+	v = v.trim().toLowerCase();
+	return v === '' ? '' : '-' + v
+})
+
+// calc tags
+let tags = new Set();
+'versatiles,ghcr.io/versatiles-org'.split(',').forEach(org => {
+	suffixes.forEach(suffix => {
+		tags.add(`${org}/${args.repo}:latest${suffix}`)
+		tags.add(`${org}/${args.repo}:${args.tag}${suffix}`)
+	})
+})
+
+args.tags = Array.from(tags.values()).join(',')
+
+for (let [key,value] of Object.entries(args)) {
+	process.stdout.write(`${key}=${value}\n`)
+}
